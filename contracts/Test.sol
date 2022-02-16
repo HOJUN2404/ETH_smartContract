d0026cddd0fc47bf054c280eac7287ee85178087
HOJUN2404 <HOJUN2404@gmail.com>
2022-02-16 15:40:09 +0900

    ETH_SmartContract

---
+++ contracts/Test.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.4.0 <0.9.2;
+
+contract TEST {
+    string public constant name = "TEST token";
+    string public constant symbol = "TEST";
+    uint256 public constant decimals = 18;
+    uint256 public constant INITIAL_SUPPLY = 1000 * 100 **decimals;
+}
